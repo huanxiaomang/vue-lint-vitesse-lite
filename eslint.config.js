83f72221a4624a2591d294e8fba4e00f9e08f76d
huanxiaomang <atri2022@163.com>
2024-07-06 21:21:31 +0800

    feat: 新增一堆好东西♂

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,29 @@
 import antfu from '@antfu/eslint-config'
 
+const OFF = 0;
+// const WARN = 1;
+const ERROR = 2;
+
 export default antfu(
   {
     unocss: true,
     formatters: true,
+
+  },
+  {
+    rules: {
+      'semi': [1, 'always'],
+      'no-console': 'off',
+      		// 对象的最后一个可以增加【,】
+      '@typescript-eslint/comma-dangle': OFF,
+
+      // 不允许使用var
+      'no-var': ERROR,
+      // 函数不需要ts标注返回类型
+      '@typescript-eslint/explicit-function-return-type': OFF,
+      'no-tabs': OFF,
+      '@typescript-eslint/indent': OFF,
+
+    },
   },
 )
--- types/data.d.ts
+++ types/data.d.ts
@@ -1,3 +1,3 @@
 export interface UserInfo {
-  userId: string;
+  userId: string
 }
